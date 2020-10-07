810d78e295980a314545460027d159a99dbe0e2b
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-07 18:18:19 +0100

    added landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,18 @@
 import React, {Fragment} from 'react';
-
+import {  Container } from 'react-bootstrap'
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <Fragment>
-      <h1>Welcome to the shop</h1>
+      <Header/>
+      <main className="py-3">
+        <Container> 
+        <h1>Welcome to the shop</h1>
+        </Container>
+        </main>
+      <Footer/> 
     </Fragment>
   );
 }
