8476320b77c23348c6115b84cc64b5205fa508da
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-08 09:05:32 +0100

    added error handler

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,34 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+const router = express.Router()
+import Product from '../models/productModel.js'
+
+
+// Fetch all products
+// GET / api/products
+// Public
+router.get('/', asyncHandler(async (req, res) => {
+    const products = await Product.find({})
+
+
+  res.json(products)
+}))
+
+// Fetch single products
+// GET / api/products/:id
+// Public
+router.get('/:id', asyncHandler(async(req, res) => {
+  const product = await Product.findById(req.params.id)
+
+  if(product) {
+    res.json(product)
+  }else {
+    res.status(404)
+    throw new Error('Product not found')
+  }
+
+  
+}
+))
+
+export default router
