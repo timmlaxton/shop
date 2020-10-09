483ffab36ef851cb4b83e14a9e0938ae35640ccb
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-09 11:00:40 +0100

    user can login

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from 'mongoose'
+import bcrypt from 'bcryptjs'
 
 const userSchema = mongoose.Schema({
   name: {
@@ -20,6 +21,20 @@ const userSchema = mongoose.Schema({
   },
 }, {
   timestamps: true
+})
+
+userSchema.methods.matchPassword = async function(enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password)
+}
+
+userSchema.pre('save', async function (next){
+  if(!this.isModified('password')) {
+    next()
+  }
+
+
+  const salt = await bcrypt.genSaltSync(10)
+  this.password = await bcrypt.hash(this.password, salt)
 })
 
 const User = mongoose.model('User', userSchema)
