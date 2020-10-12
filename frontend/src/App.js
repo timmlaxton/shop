36b99f58307d4767333c016cbd41c687f87ab937
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-12 13:44:56 +0100

    added checkout steps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from './screens/CartScreen';
 import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
+import ShippingScreen from './screens/ShippingScreen';
 
 function App() {
   return (
@@ -17,8 +18,8 @@ function App() {
       <main className="py-3">
         <Container> 
         <Route path="/login"  component={LoginScreen} />
+        <Route path="/shipping"  component={ShippingScreen} />
         <Route path="/register"  component={RegisterScreen} />
-
         <Route path="/profile"  component={ProfileScreen} />
         <Route path="/product/:id"  component={ProductScreen} />
         <Route path="/cart/:id?"  component={CartScreen} />
