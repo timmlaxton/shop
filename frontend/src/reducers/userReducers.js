641fb3cf8d420a2b88c88f20e97f5aa9e09f9480
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-12 12:17:35 +0100

    added profile screen

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -6,6 +6,9 @@ import {
   USER_REGISTER_SUCCESS,
   USER_REGISTER_REQUEST,
   USER_REGISTER_FAIL,
+  USER_DETAILS_REQUEST,
+  USER_DETAILS_SUCCESS,
+  USER_DETAILS_FAIL
 } from '../constants/userConstants'
 
 
@@ -20,6 +23,19 @@ export const userRegisterReducer = (state = {}, action) => {
         default: 
             return state
     }
+}
+
+export const userDetailsReducer = (state = {user: {} }, action) => {
+  switch (action.type) {
+      case USER_DETAILS_REQUEST:
+        return {...state, loading: true}
+      case USER_DETAILS_SUCCESS:
+        return {loading: false, user: action.payload}
+      case USER_DETAILS_FAIL:
+        return {loading: false, error: action.payload}
+      default: 
+          return state
+  }
 }
 
 export const userLoginReducer = (state = {}, action) => {
