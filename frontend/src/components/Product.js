51fc7b1b7541f4f2f4f36d6e801b66b2e5c766b5
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-12 16:14:50 +0100

    can calculate prices, tax, shipping

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -19,7 +19,7 @@ const Product = ({product}) => {
           <Card.Text as='div'>
             <Rating value={product.rating} text={`${product.numReviews} reviews`}/>
           </Card.Text>
-  <Card.Text as='h3'>${product.price}</Card.Text>
+  <Card.Text as='h3'>£{product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
--- frontend/src/screens/PaymentScreen.js
+++ frontend/src/screens/PaymentScreen.js
@@ -22,7 +22,7 @@ const PaymentScreen = ({history}) => {
   const submitHandler = (e) => {
     e.preventDefault()
     dispatch(savePaymentMethod({paymentMethod}))
-    history.push('/placeOrder')
+    history.push('/placeorder')
   }
 
   return <FormContainer> 
