f7fc58d19e1aa11cea715a16ef2bf937ae3d0c46
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-12 11:29:35 +0100

    can update user profile

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,21 +2,37 @@ import {
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
   USER_LOGOUT,
-  USER_LOGIN_FAIL
+  USER_LOGIN_FAIL,
+  USER_REGISTER_SUCCESS,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_FAIL,
 } from '../constants/userConstants'
 
 
-export const userLoginReducer = (state = {}, action) => {
+export const userRegisterReducer = (state = {}, action) => {
     switch (action.type) {
-        case USER_LOGIN_REQUEST:
+        case USER_REGISTER_REQUEST:
           return {loading: true}
-        case USER_LOGIN_SUCCESS:
+        case USER_REGISTER_SUCCESS:
           return {loading: false, userInfo: action.payload}
-        case USER_LOGIN_FAIL:
+        case USER_REGISTER_FAIL:
           return {loading: false, error: action.payload}
-        case USER_LOGOUT:
-          return {}
         default: 
             return state
     }
+}
+
+export const userLoginReducer = (state = {}, action) => {
+  switch (action.type) {
+      case USER_LOGIN_REQUEST:
+        return {loading: true}
+      case USER_LOGIN_SUCCESS:
+        return {loading: false, userInfo: action.payload}
+      case USER_LOGIN_FAIL:
+        return {loading: false, error: action.payload}
+      case USER_LOGOUT:
+        return {}
+      default: 
+          return state
+  }
 }
