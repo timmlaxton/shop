5d4591132ecf7e2afbba71374fd4bd2946b0a947
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-13 08:34:12 +0100

    order screen added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import ProfileScreen from './screens/ProfileScreen';
 import ShippingScreen from './screens/ShippingScreen';
 import PaymentScreen from './screens/PaymentScreen';
 import PlaceOrderScreen from './screens/PlaceOrderScreen';
+import OrderScreen from './screens/OrderScreen';
 
 function App() {
   return (
@@ -20,6 +21,7 @@ function App() {
       <main className="py-3">
         <Container> 
         <Route path="/login"  component={LoginScreen} />
+        <Route path="/order/:id"  component={OrderScreen} />
         <Route path="/shipping"  component={ShippingScreen} />
         <Route path="/payment"  component={PaymentScreen} />
         <Route path="/placeorder"  component={PlaceOrderScreen} />
