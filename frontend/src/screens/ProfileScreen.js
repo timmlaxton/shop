edda7e6069a2f1793fceea7ce21db4b4cc8f6042
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-16 13:59:47 +0100

    profile resets after logout

--- frontend/src/screens/ProfileScreen.js
+++ frontend/src/screens/ProfileScreen.js
@@ -1,5 +1,6 @@
 import React, {useState, useEffect} from 'react'
-import {Form, Button, Row, Col} from 'react-bootstrap';
+import {Form, Button, Row, Col, Table} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap'
 import {useDispatch, useSelector} from 'react-redux'
 import Message from '../components/Message'
 import Loader from '../components/Loader'
@@ -26,7 +27,7 @@ const ProfileScreen = ({location, history}) => {
   const { success } = userUpdateProfile
 
   const orderListMy = useSelector(state => state.orderListMy)
-  const { loading: loadingorders, error: errorOrders, orders} = orderListMy
+  const { loading: loadingOrders, error: errorOrders, orders} = orderListMy
 
 
   useEffect(() => {
@@ -111,7 +112,7 @@ const ProfileScreen = ({location, history}) => {
     <h2>My Orders</h2>    
     {loadingOrders ? <Loader /> : errorOrders ? <Message variant='danger'>
     {errorOrders}</Message> : (
-      <Table striped bordered hover responsive className='table-sm'>
+      <Table striped bordered hover responsive className='table-sm'> 
         <thead>
           <tr>
             <th>ID</th>
@@ -126,18 +127,27 @@ const ProfileScreen = ({location, history}) => {
           {orders.map(order => (
             <tr key={order._id}>
               <td>{order._id}</td>
-              <td>{order.createdAt.substring(0,10)}</td>
+              <td>{order.createdAt.substring(0, 10)}</td>
               <td>{order.totalPrice}</td>
-              <td>{order.isPaid ? (
-                order.paidAt.substring(0,10)  ) : (
-                <i className='fas fa-times' style={{color: red}}></i> 
+              <td>{order.isPaid ? order.paidAt.substring(0, 10) : (
+                <i className='fas fa-times' style={{color: 'red'}}></i> 
               )}
               </td>
-              <td>{order._id}</td>
+              <td>{order.isDelivered ? (
+                order.deliveredAt.substring(0, 10) 
+                ) : (
+                <i className='fas fa-times' style={{color: 'red'}}></i> 
+              )}
+              </td>
+              <td> 
+              <LinkContainer to={`order/${order._id}`}>
+                <Button className='btn-sm' variant='light'>Details</Button>
+              </LinkContainer>
+             </td>
             </tr>
           ))}
         </tbody>
-      </Table>
+        </Table>
     )}
     </Col>
   </Row>
