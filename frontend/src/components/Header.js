bf3705fafc0286191eb3ffcb3e8744b94055eb18
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-18 11:03:27 +0100

    added product list

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -46,10 +46,10 @@ const Header = () => {
         )}
          {userInfo && userInfo.isAdmin && (
            <NavDropdown title='Admin' id='adminmenu'>
-           <LinkContainer to='admin/userlist'>
+           <LinkContainer to='/admin/userlist'>
              <NavDropdown.Item>Users</NavDropdown.Item>
           </LinkContainer>
-          <LinkContainer to='admin/productist'>
+          <LinkContainer to='/admin/productlist'>
              <NavDropdown.Item>Products</NavDropdown.Item>
           </LinkContainer>
           <LinkContainer to='admin/orderist'>
