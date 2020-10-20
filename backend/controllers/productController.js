b9f3d56fae39193a8628a45da01d9f5cf26a6926
Tim Laxton <49490170+timmlaxton@users.noreply.github.com>
2020-10-20 12:00:15 +0100

    pagination added to product and admin pages

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -5,6 +5,8 @@ import Product from '../models/productModel.js'
 // GET / api/products
 // Public
 const getProducts = asyncHandler (async (req, res) => {
+  const pageSize = 5
+  const page = Number(req.query.pageNumber) || 1
   const keyword = req.query.keyword ? {
     name: {
       $regex: req.query.keyword,
@@ -12,10 +14,10 @@ const getProducts = asyncHandler (async (req, res) => {
     }
   } : {}
 
+  const count = await Product.countDocuments({...keyword})
+  const products = await Product.find({...keyword}).limit(pageSize).skip(pageSize * (page -1))
 
-  const products = await Product.find({...keyword})
-
-  res.json(products)
+  res.json({products, page, pages: Math.ceil(count / pageSize)})
 })
 
 // Fetch single products
